46c7880e5a5022ccf38bf3ba8bb959949fae29b8
Anthony Squire <squire.anthony@gmail.com>
2024-02-20 13:36:06 +0000

    feat: generated random number, set guess_count and username, ensured username not >22 characters long

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+RANDOM_NUMBER=$((1 + $RANDOM % 1000))
+GUESS_COUNT=0
+
+echo "Enter your username:"
+read USERNAME
+
+while [[ ${#USERNAME} -gt 22 ]]
+do
+  echo "That username is too long, please enter a shorter one:"
+  read USERNAME
+done
