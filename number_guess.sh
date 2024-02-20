bb861025088e884f1af877068773479d9de0155f
Anthony Squire <squire.anthony@gmail.com>
2024-02-20 13:47:20 +0000

    fix: added prevention of no username being entered

--- number_guess.sh
+++ number_guess.sh
@@ -8,8 +8,13 @@ GUESS_COUNT=0
 echo "Enter your username:"
 read USERNAME
 
-while [[ ${#USERNAME} -gt 22 ]]
+while [[ ${#USERNAME} -gt 22 || -z $USERNAME ]] 
 do
-  echo "That username is too long, please enter a shorter one:"
+  if [[ ${#USERNAME} -gt 22 ]]
+  then
+    echo "That username is too long, please enter a shorter one:"
+  else
+    echo "Sorry something went wrong, please re-enter your username:" 
+  fi
   read USERNAME
 done
